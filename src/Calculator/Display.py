3de3e1d1d910167ba2d1955cba26dc1170d2361f
pratik-stha <pratikstha440@gmail.com>
2020-12-15 13:38:04 -0500

    Disabled Keyboard input

--- src/Calculator/Display.py
+++ src/Calculator/Display.py
@@ -5,7 +5,8 @@ class Display:
         self.operator=""
         self.textin = StringVar(master)
         self.textin.set("")
-        self.metext=Entry(master,font=("Courier New",12,'bold'),textvariable=self.textin,width=25,bd=5,bg='powder blue')
+        self.metext=Entry(master,font=("Courier New",12,'bold'),textvariable=self.textin,width=25,bd=5,bg='powder blue' )
+        self.metext.bind("<Key>", lambda e: "break") 
         self.metext.pack()
 
         
